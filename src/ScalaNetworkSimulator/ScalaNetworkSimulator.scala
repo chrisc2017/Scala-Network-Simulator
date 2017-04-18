b2c49dedc6b712364c50debd87e406b73d390262
chris <superchris@wireless-10-147-185-226.public.utexas.edu>
2017-04-18 18:20:22 -0500

    first commit

---
+++ src/ScalaNetworkSimulator/ScalaNetworkSimulator.scala
@@ -0,0 +1,15 @@
+package ScalaNetworkSimulator
+
+object ScalaNetworkSimulator {
+  
+  def main( args: Array[String] ){
+    
+    
+    println("hello world")
+    
+    
+    
+    
+    
+  }
+}
