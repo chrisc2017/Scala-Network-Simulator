2c47517235147ea65336a2c1030d4ec78a6e9196
Rambo Wu <wurambo@utexas.edu>
2017-04-25 16:41:13 -0500

    scalaNetworkSimulator now has a template program

--- src/ScalaNetworkSimulator/ScalaNetworkSimulator.scala
+++ src/ScalaNetworkSimulator/ScalaNetworkSimulator.scala
@@ -4,10 +4,23 @@ object ScalaNetworkSimulator  extends NetworkSimulator{
   
   def main( args: Array[String] ){
     
+    RoutingProtocol name "rp1";
+      learn by "bandwith";
+      choose port "min";
+      
     Switch name "sw3";
       port num 5 uses "Fiber";
-    Switch name "sw3";
       
+    Router name "r3";
+      port num 8 uses "Ethernet";
+      
+    PC name "pc1";
+      port num 9 uses "Fiber";
+      
+    Links;
+      connect deviceA "sw3" portA 5 deviceB "pc1" portB 9;
+      
+    print("everything worked!")
     
     
   }
