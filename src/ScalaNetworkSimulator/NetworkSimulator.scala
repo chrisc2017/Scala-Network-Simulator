d1bacc3751d0d21aeeb03870f1550a15973c0376
chris <superchris@iBlue.T-mobile.com>
2017-05-03 00:40:15 -0500

    corrected pdu.packet indicies for ARPrequest method

--- src/ScalaNetworkSimulator/NetworkSimulator.scala
+++ src/ScalaNetworkSimulator/NetworkSimulator.scala
@@ -301,15 +301,15 @@ class NetworkSimulator {
   //this request can be sent from Routers or PC's
   def requestARP(pdu: PDU){
     
-    if( pdu.packet(6).isInstanceOf[SwitchClass] ){//if the current device the packet is on is a switch
+    if( pdu.packet(8).isInstanceOf[SwitchClass] ){//if the current device the packet is on is a switch
   
-      for(counter <- 1 to pdu.packet(6).asInstanceOf[SwitchClass].ports.size ){
+      for(counter <- 1 to pdu.packet(8).asInstanceOf[SwitchClass].ports.size ){
         
-        if(pdu.packet(6).asInstanceOf[SwitchClass].ports.get(counter) != pdu.packet(5).asInstanceOf[PortClass] ){
+        if(pdu.packet(8).asInstanceOf[SwitchClass].ports.get(counter) != pdu.packet(7).asInstanceOf[PortClass] ){
           
           //update PDU(5) and PDU(6) to the port# and device ref of the port on the opposite side of the Link
-          pdu.packet(5) = globalLinksTable.get( pdu.packet(6).asInstanceOf[SwitchClass].ports.get(counter).get )//set next port on link as the current port
-          pdu.packet(6) = globalLinksTable.get( pdu.packet(6).asInstanceOf[SwitchClass].ports.get(counter).get ).get.device //set next port's device as the current device
+          pdu.packet(7) = globalLinksTable.get( pdu.packet(8).asInstanceOf[SwitchClass].ports.get(counter).get )//set next port on link as the current port
+          pdu.packet(8) = globalLinksTable.get( pdu.packet(8).asInstanceOf[SwitchClass].ports.get(counter).get ).get.device //set next port's device as the current device
           println("forwarding ARP request out of port " + counter.toString())
           requestARP(pdu)//causes recursive call to flood all switch ports with an ARP request
         }
@@ -321,8 +321,8 @@ class NetworkSimulator {
     
     //if we get here in the code then we have either reached a PC or a router
     //check if this PC's port has the IP address of the packet's destination IP
-    if( pdu.packet(5).asInstanceOf[PortClass].IPAddr == pdu.packet(1) ){
-          println(pdu.packet(6).asInstanceOf[PCClass].name + " successfully received the ARP request. Sending reply to "  )
+    if( pdu.packet(7).asInstanceOf[PortClass].IPAddr == pdu.packet(1) ){
+          println(pdu.packet(8).asInstanceOf[PCClass].name + " successfully received the ARP request. Sending reply to "  )
     }
     else{
       println("Dropping the packet because the destination IP address does not match its IP address")
